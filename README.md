042a8c1bba0e6776601fb3a61f8e9c3c105860de
Cong Le <longchike@gmail.com>
2022-08-27 14:40:30 -0700

    - Update the README file -  Fully present the embedded SwiftUIView onto the UIView controller

--- README.md
+++ README.md
@@ -4,4 +4,36 @@ A sample project demonstrate 3 methods to present a SwiftUI View on an existing 
 ## Video Demo
 
 
-<img src="https://user-images.githubusercontent.com/31250006/130022854-48e401c3-3ecb-471c-9286-c96b97d5977e.gif" width="300" height="600" />
+<img src="https://user-images.githubusercontent.com/31250006/130022854-48e401c3-3ecb-471c-9286-c96b97d5977e.gif" width="300" height="600"/>
+
+
+#Using SwiftUI with UIKit
+* UIHostingController 
+* Bridging data 
+* SwiftUI in cells 
+* Data flow for cells
+
+### UIHostingController
+
+- UIHostingController is a UIViewController that contains a SwiftUI view hierarchy. 
+- We can use a hosting controller anywhere we can use a view controller in UIKit.
+- Structure of UIHostingController: UIHostingController is a view controller, which means it has a UIView stored in its view property, and inside that view is where the SwiftUI content is rendered.
+
+<img src="./Images/UIHostingController.png" width="600" height="600"/>
+
+- How to use UIHostingController:
+
+```swift
+// Presenting a UIHostingController
+let swiftUIView = SwiftUIView()
+let hostingController = UIHostingController(rootView: swiftUIView)
+
+// Present the hosting controller modally
+self.present(hostingController, animated: true, completion: nil)
+```
+
+
+
+
+
+
--- SwiftUIViewForUIKitProject/ViewController.swift
+++ SwiftUIViewForUIKitProject/ViewController.swift
@@ -10,115 +10,120 @@ import UIKit
 import SwiftUI /// contains instance `UIHostingController`
 
 class ViewController: UIViewController {
-  
-  lazy var buttonToPresentSwiftUIView: UIButton = {
-    let button = UIButton(type: .system)
-    button.backgroundColor = .yellow
-    button.setTitle("Present SwiftUI View", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.cornerRadius = 5.0
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.addTarget(self, action: #selector(presentSwiftUIView), for: .touchUpInside)
-    return button
-  }()
-  
-  lazy var buttonToAddSwiftUIViewToUIKitView: UIButton = {
-    let button = UIButton(type: .system)
-    button.backgroundColor = .yellow
-    button.setTitle("Add SwiftUI View", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.cornerRadius = 5.0
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.addTarget(self, action: #selector(addSwiftUIView), for: .touchUpInside)
-    return button
-  }()
-  
-  lazy var buttonToAddSwiftUIViewToUIKitViewThroughExtension: UIButton = {
-    let button = UIButton(type: .system)
-    button.backgroundColor = .yellow
-    button.setTitle("Add SwiftUI View via extension", for: .normal)
-    button.setTitleColor(.black, for: .normal)
-    button.layer.cornerRadius = 5.0
-    button.translatesAutoresizingMaskIntoConstraints = false
-    button.addTarget(self, action: #selector(addSwiftUIView), for: .touchUpInside)
-    return button
-  }()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .red
-    navigationItem.title = "UIViewController Title"
-    navigationItem.largeTitleDisplayMode = .always
     
-    // Button to present the SwiftUI view
-    view.addSubview(buttonToPresentSwiftUIView)
+    lazy var buttonToPresentSwiftUIView: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.setTitle("Present SwiftUI View", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(presentSwiftUIView), for: .touchUpInside)
+        return button
+    }()
     
-    // Button to add SwiftUI View
-    view.addSubview(buttonToAddSwiftUIViewToUIKitView)
+    lazy var buttonToAddSwiftUIViewToUIKitView: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.setTitle("Add SwiftUI View", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addSwiftUIView), for: .touchUpInside)
+        return button
+    }()
     
-    // Button to add SwiftUI view from extension of UIViewController
-    view.addSubview(buttonToAddSwiftUIViewToUIKitViewThroughExtension)
+    lazy var buttonToAddSwiftUIViewToUIKitViewThroughExtension: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .yellow
+        button.setTitle("Add SwiftUI View via extension", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.layer.cornerRadius = 5.0
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(addSwiftUIViewViaExtension), for: .touchUpInside)
+        return button
+    }()
     
-    setupConstraints()
-  }
-  
-  func setupConstraints() {
-    NSLayoutConstraint.activate([
-      buttonToPresentSwiftUIView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      buttonToPresentSwiftUIView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-      buttonToAddSwiftUIViewToUIKitView.topAnchor.constraint(equalTo: buttonToPresentSwiftUIView.bottomAnchor, constant: 20),
-      buttonToAddSwiftUIViewToUIKitView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      buttonToAddSwiftUIViewToUIKitViewThroughExtension.topAnchor.constraint(equalTo: buttonToAddSwiftUIViewToUIKitView.bottomAnchor, constant: 20),
-      buttonToAddSwiftUIViewToUIKitViewThroughExtension.centerXAnchor.constraint(equalTo: view.centerXAnchor)
-    ])
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        navigationItem.title = "UIViewController Title"
+        navigationItem.largeTitleDisplayMode = .always
+        
+        // Button to present the SwiftUI view
+        view.addSubview(buttonToPresentSwiftUIView)
+        
+        // Button to add SwiftUI View
+        view.addSubview(buttonToAddSwiftUIViewToUIKitView)
+        
+        // Button to add SwiftUI view from extension of UIViewController
+        view.addSubview(buttonToAddSwiftUIViewToUIKitViewThroughExtension)
+        
+        setupConstraints()
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            buttonToPresentSwiftUIView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonToPresentSwiftUIView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonToAddSwiftUIViewToUIKitView.topAnchor.constraint(equalTo: buttonToPresentSwiftUIView.bottomAnchor, constant: 20),
+            buttonToAddSwiftUIViewToUIKitView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonToAddSwiftUIViewToUIKitViewThroughExtension.topAnchor.constraint(equalTo: buttonToAddSwiftUIViewToUIKitView.bottomAnchor, constant: 20),
+            buttonToAddSwiftUIViewToUIKitViewThroughExtension.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
 }
 
 // MARK: - Different methods to display SwiftUI view into UIKit view controller
 extension ViewController {
-  
-  // Preseting a SwiftUI view in UIKit View Controller
-  @objc func presentSwiftUIView() {
-    let swiftUIView = SwiftUIView()
-    let hostingController = UIHostingController(rootView: swiftUIView)
-    present(hostingController, animated: true, completion: nil)
-  }
-  
-  // Add a SwiftUI view to a UIKit View
-  @objc func addSwiftUIView() {
-    let swiftUIView = SwiftUIView()
-    let hostingController = UIHostingController(rootView: swiftUIView)
-    hostingController.modalTransitionStyle = .crossDissolve
-    navigationController?.pushViewController(hostingController, animated: true)
-  }
-  
-  @objc func addSwiftUIViewViaExtension() {
-    self.addSwiftUIView()
-  }
+    
+    /// Preseting a SwiftUI view in UIKit View Controller
+    @objc func presentSwiftUIView() {
+        let swiftUIView = SwiftUIView()
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        // Present the hosting controller modally
+        present(hostingController, animated: true, completion: nil)
+    }
+    
+    /// Add a SwiftUI view to a UIKit View
+    @objc func addSwiftUIView() {
+        let swiftUIView = SwiftUIView()
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        
+        hostingController.modalTransitionStyle = .crossDissolve
+        navigationController?.pushViewController(hostingController, animated: true)
+    }
+    
+    @objc func addSwiftUIViewViaExtension() {
+        let swiftUIVIew = SwiftUIView()
+        self.addSubSwiftUIView(swiftUIVIew, to: view)
+    }
 }
 
 //MARK: - UIViewController Extension
 extension UIViewController {
-  // Fully add SwiftUI view onto the UIKit view
-  func addSubSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content: View {
-    let hostingController = UIHostingController(rootView: swiftUIView)
-    // add a child of the current view controller
-    addChild(hostingController)
-    
-    // add the SwiftUI view to the view controller view hierarchy
-    view.addSubview(hostingController.view)
-    
-    // Setup the constraints to update the SwiftUI view boundaries
-    hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-    
-    NSLayoutConstraint.activate([
-      hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
-      hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
-      view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
-      view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
-    ])
-    
-    // Notify the hosting controller that it has been moved to the current view controller
-    hostingController.didMove(toParent: self)
-  }
+    /// Fully add SwiftUI `view` onto the UIKit `view`
+    /// - Parameters:
+    ///   - swiftUIView: The SwiftUI `view` to add as a child.
+    ///   - view: The `UIView` instance to which the view should be added.
+    func addSubSwiftUIView<Content>(_ swiftUIView: Content, to view: UIView) where Content: View {
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        /// Add the hosting controller as a child view controller for the current view controller
+        self.addChild(hostingController)
+        
+        /// add the SwiftUI view to the view controller view hierarchy
+        self.view.addSubview(hostingController.view)
+        
+        /// Setup the constraints to update the SwiftUI view boundaries
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor),
+            hostingController.view.leftAnchor.constraint(equalTo: view.leftAnchor),
+            view.bottomAnchor.constraint(equalTo: hostingController.view.bottomAnchor),
+            view.rightAnchor.constraint(equalTo: hostingController.view.rightAnchor)
+        ])
+        
+        /// Notify the hosting controller that it has been moved to the current view controller
+        hostingController.didMove(toParent: self)
+    }
 }
